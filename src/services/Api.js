ede48589c37c1246087224bd364890b66f1f7ccc
JFontes <jfontes.dev@gmail.com>
2021-02-22 12:47:28 +0100

    ""

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  /* baseURL: 'http://localhost:3001', */
-  baseURL: 'http://192.168.1.4:3001',
+  baseURL: process.env.REACT_APP_API_URL,
+  /*   baseURL: 'http://192.168.1.4:3001', */
 });
 
 export default api;
