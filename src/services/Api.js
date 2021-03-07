1b40592362e94ee76732604a25a682883db98031
JFontes <jfontes.dev@gmail.com>
2021-03-07 22:51:51 +0100

    env from process to window

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: window.env.REACT_APP_API_URL,
   /*   baseURL: 'http://192.168.1.4:3001', */
 });
 
--- src/utils/Cipher.js
+++ src/utils/Cipher.js
@@ -2,13 +2,19 @@ import crypto from 'crypto';
 import env from 'react-dotenv';
 
 const encrypt = (str) => {
-  const mykey = crypto.createCipher(env.ALGHORITHM, env.APP_SECRET);
+  const mykey = crypto.createCipher(
+    window.env.ALGHORITHM,
+    window.env.REACT_APP_APP_SECRET
+  );
   let mystr = mykey.update(str, 'utf8', 'hex');
   mystr += mystr.final('hex');
   return mystr;
 };
 const decrypt = (str) => {
-  const mykey = crypto.createDecipher(env.ALGHORITHM, env.APP_SECRET);
+  const mykey = crypto.createDecipher(
+    window.env.ALGHORITHM,
+    window.env.REACT_APP_APP_SECRET
+  );
   let mystr = mykey.update(str, 'hex', 'utf8');
   mystr += mystr.final('utf8');
   return mystr;
