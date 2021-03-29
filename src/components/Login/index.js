bfc2b30f7355c2fc3aa360674afb47125f63dee7
JFontes <JFontes.dev@gmail.com>
2021-03-29 11:21:24 +0100

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -20,7 +20,7 @@ export default function Login() {
         if (token !== undefined) {
           // Set object into localStorage
           localStorage.setItem('definicoes', JSON.stringify(definicoes));
-
+          console.log('login:', definicoes);
           localStorage.setItem('token', JSON.stringify(token));
           localStorage.setItem('name', name);
           window.location.href = '/home';
